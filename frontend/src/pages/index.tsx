5975785dd58c838aa323d678c243dd63dd891a21
Ivowainer <ivancamposwainer@gmail.com>
2023-10-09 12:49:37 -0300

    Next Frontend Project

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+    return (
+        <main>
+            {/* <Image src="/people_talking.jpg" className="w-2/3" alt="Decorative Image Aside Login People Talking"></Image>
+            <div className=""></div> */}
+        </main>
+    );
+}
